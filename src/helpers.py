84f9266a02f1900cb95b1fbde1cd7c4fd25a76d3
hassanahmad2136 <35006202+hassanahmad2136@users.noreply.github.com>
2025-01-14 20:00:48 +0500

    Update helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -1,8 +1,9 @@
 import csv
 import re
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
-def save_to_csv(names, reviews, info, websites, links, file_name='output.csv'):
+def save_to_csv(names, reviews, info, websites, links, file_name='../output/output.csv'):
     """
     Save the scraped data to a CSV file.
     """
@@ -12,22 +13,3 @@ def save_to_csv(names, reviews, info, websites, links, file_name='output.csv'):
         for row in zip(names, reviews, info, websites, links):
             writer.writerow(row)
     print(f"CSV file '{file_name}' has been created.")
-
-def get_element_text(driver, selector, attribute="innerText"):
-    """
-    Retrieve text or an attribute from an element, handling exceptions.
-    """
-    try:
-        element = driver.find_element_by_css_selector(selector)
-        if attribute == "innerText":
-            return element.text
-        return element.get_attribute(attribute)
-    except NoSuchElementException:
-        return ""
-
-def clean_text(text):
-    """
-    Clean text by removing special characters and unnecessary whitespace.
-    """
-    cleaned_text = re.sub(r'[^a-zA-Z0-9+\.\-\n]+', '', text)
-    return re.sub(r'\n', ' ', cleaned_text)
