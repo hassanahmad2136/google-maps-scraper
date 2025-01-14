b92dcf5950556373dfe990917da663fb1ed3f6d3
hassanahmad2136 <35006202+hassanahmad2136@users.noreply.github.com>
2025-01-14 14:53:58 +0500

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,15 @@
+# constants.py
+
+from selenium.webdriver.chrome.options import Options
+
 SEARCH_QUERY = "Clothing, New York City, NY"
+
+# Chrome options setup
+def get_chrome_options():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    return chrome_options
